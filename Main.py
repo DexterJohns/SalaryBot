418b9e523b804284403dfa57ce37a788f94d3dff
DexterJohns <streammaster@gmail.com>
2024-01-23 19:15:43 +0100

    Adding tax caountin function

--- Main.py
+++ Main.py
@@ -9,6 +9,8 @@ HEADERS = {'User-Agent': 'Mozilla/4.0 (iPad; CPU OS 12_2 like Mac OS X) AppleWeb
 bot = telebot.TeleBot('5673337125:AAGQPKmpMk_M9kwGuqMVB5BId_87IhW7jWU')
 raw_date = datetime.datetime.now()
 param_date=raw_date.strftime("%Y-%m-%d")
+tax_percent = 0.15                                  #This is tax in Budva
+salary_gross = 0.7                                  #This is 70% of salary in USD we should pay tax for
 
 
 @bot.message_handler(commands=['start'])
@@ -23,17 +25,14 @@ def get_user_text(message):
     
     if message.text == "/srv":
         bot.send_message(message.chat.id, message, 'lxml')
-
     
     elif mess.isdigit():
-        global salary
-        global rate
-        global data
-        salary = message.text
+        global salary_usd, salary_eur, rate
+        salary_usd = int(message.text)
         #print (param_date)
         base_url = "https://www.cbcg.me/en/core-functions/financial-and-banking-operations/fx-reference-rates?vazi_od=" + param_date
         #base_url = "https://www.cbcg.me/download.php?date=" + param_date
-        print (base_url)
+        #print (base_url)
         r= requests.get(base_url, verify=False, timeout=5)
         soup = BeautifulSoup(r.content, "html.parser")
         #print (soup)
@@ -41,12 +40,12 @@ def get_user_text(message):
         str1 = "".join(map(str,soup.find(lambda tag: tag.name == 'tr' and 'USD' in tag.text)))
         str2 = "".join(line.strip() for line in str1.splitlines())
         str3 = re.search(r"(\d).(\d{5})",str2) 
-        rate = str3.group(0)
-        write_to_file()
+        rate = float(str3.group(0))
+        salary_eur = int(salary_usd/rate)
+        taxes= int(count_tax(salary_eur))
+        write_to_file(taxes)
         lines_left=count_lines()
-        bot.send_message(message.chat.id, f"Date:                   {param_date} \nSalary $:              {salary}  \nRate USD-EUR:  {rate} \n\n{lines_left} records in the /list.", parse_mode='html')
-        
-
+        bot.send_message(message.chat.id, f"Date:                   {param_date} \nSalary $:              {salary_usd}  \nRate USD-EUR:   {rate} \nTaxes €:               {taxes}\n\n{lines_left} records in the /list       /rm.", parse_mode='html')
 
     elif message.text == "/rate":
         #bot.send_message(message.from_user.id, f"PRVA:   {get_PRVA_rates()} \nLOVC:  {get_LOVCEN_rates()} \nHIPO:   {get_HIPO_rates()} \nERST:   {get_ERSTE_rates()}\nNLB:    {get_NLB_rates()}", parse_mode='html')
@@ -76,16 +75,22 @@ def get_user_text(message):
         #get_ZIRAAT_rates()
         
 
-def write_to_file():
+def write_to_file(tax_2pay):
     file = open('Salary_log.txt', "a")
     #current_datetime = datetime.now()
     file.write(str(param_date))
     file.write('  ')
-    file.write(str(salary))
+    file.write(str(salary_usd))
     file.write('  ')
     file.write(str(rate))
+    file.write('  ')
+    file.write(str(tax_2pay))
     file.write('\n')
     file.close()
+
+def count_tax(sal_eur):
+    tax = int(sal_eur*tax_percent*salary_gross)
+    return tax
 
 def count_lines():
     with open(r"Salary_log.txt", 'r') as fp:
@@ -210,8 +215,6 @@ def get_year_list(year):
         # Print the line, if the given string is found in the current line
                 year_list=year_list+line.strip()+'\n'
                 #print(line.strip())
-
-
                 print (year_list)
     #return year_list
 
